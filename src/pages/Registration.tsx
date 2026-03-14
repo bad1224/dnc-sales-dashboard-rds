f226934541788aab3cf87a344270640dbc62496d
Rudson Reiler Silva Santana <alzze36@gmail.com>
2026-03-14 17:35:31 -0300

    Salvando ajustes de configuração tsconfig

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-                                                        
+import{BannerImage} from '@components'                                                     
 const LoginArea = styled.div `
     background: #333;
 
